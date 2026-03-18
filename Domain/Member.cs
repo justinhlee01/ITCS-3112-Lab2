a49f3e10179cd64fc7e3c68655de5e432242d16e
bstahlman27 <bstahlman27@gmail.com>
2026-03-18 17:10:46 -0400

    Updated Member class and made AuthService

--- Domain/Member.cs
+++ Domain/Member.cs
@@ -2,20 +2,20 @@ namespace ITCS_3112_Lab2.Domain;
 
 public class Member
 {
-    public string Account { get; set; }
+    public string Email { get; set; }
     public string Name { get; set; }
     public string Password { get; set; }
 
-    public Member(string account, string name, string password)
+    public Member(string email, string name, string password)
     {
-        if (string.IsNullOrEmpty(account))
+        if (string.IsNullOrEmpty(email))
             throw new ArgumentNullException(nameof(account));
         if (string.IsNullOrEmpty(name))
             throw new ArgumentNullException(nameof(name));
         if (string.IsNullOrEmpty(password))
             throw new ArgumentNullException(nameof(password));
 
-        Account = account;
+        Email = email;
         Name = name;
         Password = password;
     }
--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,6 +1,32 @@
+using ITCS_3112_Lab2.Contracts;
+using ITCS_3112_Lab2.Domain;
+using ITCS_3112_Lab2.Repositories;
+
 namespace ITCS_3112_Lab2.Services;
 
 public class AuthService
 {
-    
+    private readonly IBookRepository _bookRepository;
+    private readonly IMemberRepository _memberRepository;
+    private readonly IRatingRepository _ratingRepository;
+    bool isLoggedIn = false;
+    public AuthService(IBookRepository bookRepository, IMemberRepository memberRepository, IRatingRepository ratingRepository)
+    {
+        _bookRepository = bookRepository;
+        _memberRepository = memberRepository;
+        _ratingRepository = ratingRepository;
+    }
+
+    public void login(string email, string password)
+    {
+        var member = _memberRepository.GetMemberById(email);
+        if (member == null) throw new ArgumentException("Email not found");
+        if (member.Password != password) throw new ArgumentException("Passwords do not match");
+        isLoggedIn = true;
+    }
+
+    public void logout()
+    {
+        isLoggedIn = false;
+    }
 }
