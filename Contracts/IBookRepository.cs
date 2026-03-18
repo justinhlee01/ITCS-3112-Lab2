fd47a542fedf0f8a8581f2fcb817788bfab6a482
“Justin” <jlee356@uncc.edugit config --global user.email jlee356@uncc.edu>
2026-03-18 16:43:04 -0400

    add interfaces

--- Contracts/IBookRepository.cs
+++ Contracts/IBookRepository.cs
@@ -1,6 +1,12 @@
+using ITCS_3112_Lab2.Domain;
+
 namespace ITCS_3112_Lab2.Contracts;
 
 public interface IBookRepository
 {
-    
+    Book Addbook(Book book);
+    void Removebook(Book book);
+    Book Updatebook(Book book);
+    Book GetBookById(int bookId);
+    Book GetBookAllBooks(string title);
 }
--- Contracts/IMemberRepository.cs
+++ Contracts/IMemberRepository.cs
@@ -1,6 +1,12 @@
+using ITCS_3112_Lab2.Domain;
+
 namespace ITCS_3112_Lab2.Contracts;
 
 public interface IMemberRepository
 {
-    
+    Member AddMember(Member member);
+    void RemoveMember(Member member);
+    Member UpdateMember(Member member);
+    Member GetMemberById(string email);
+    Member GetMemberAllMembers(string email);
 }
